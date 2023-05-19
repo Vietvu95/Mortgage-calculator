c89eb3df57b5eb4ca19738a5d194e199e921123d
vietvu <viet.vu.95@hotmail.com>
2023-05-19 20:12:00 +1000

    created app and added components

---
+++ src/components/SliderSelect.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+import SliderComponent from './common/SliderComponent'
+
+
+const SliderSelect = () => {
+  return (
+    <Fragment>
+      <SliderComponent min={0} max={50} defaultValue={20}/>
+      <SliderComponent min={100} max={200} defaultValue={150}/>
+      <SliderComponent min={0} max={50} defaultValue={20}/>
+    </Fragment>
+  )
+}
+
+export default SliderSelect
---
+++ src/components/TenureSelect.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TenureSelect = () => {
+  return (
+    <div>tensureSelect</div>
+  )
+}
+
+export default TenureSelect
