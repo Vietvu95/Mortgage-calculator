ab6f28a22033e253d6339ea0e1481410e62a4714
vietvu <viet.vu.95@hotmail.com>
2023-05-22 19:24:39 +1000

    mastered sliders

--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Slider from '@mui/material/Slider';
+import { Typography } from '@mui/material';
 
-const SliderComponent = (props) => {
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value }) => {
   return (
-     <Slider defaultValue={props.defaultValue} min={props.min} max={props.max} aria-label="Default" valueLabelDisplay="auto" />
-  )
-}
+    <>
+      <Typography variant="subtitle2">Home Value</Typography>
+      <Typography variant ="h5">$3000</Typography>
+      <Slider defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+        step={step}
+        onChange={onChange}
+        value={value}
+      />
+      <Typography></Typography>
+    </>
+    //fake dom. - when parent error occurs
+    //same works with <react.fragment> </react.fragment> but needs  import React from 'react'
+  );
+};
 
 export default SliderComponent
